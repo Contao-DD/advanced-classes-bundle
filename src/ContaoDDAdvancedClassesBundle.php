bb9f07840701d869e21dca4eb5c2410d366e4317
Marcel Rableski <marcel.rableski@ressourcenmangel.de>
2015-06-06 12:42:45 +0200

    add Class

---
+++ src/ContaoDDAdvancedClassesBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+  /**
+   * Contao Open Source CMS
+   *
+   * Copyright (c) 2015 Contao Stammtisch Dresden
+   * @package advanced-classes
+   * @author Mathias Arzberger <develop@pdir.de>
+   * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+   */
+
+  namespace ContaoDD\AdvancedClassesBundle;
+
+  use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+  /**
+   * Configures the Contao advanced class bundle.
+   *
+   * @author Leo Feyer <https://github.com/leofeyer>
+   */
+  class ContaoDDAdvancedClassesBundle extends Bundle
+  {
+  }
