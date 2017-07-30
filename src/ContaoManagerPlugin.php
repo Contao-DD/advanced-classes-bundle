44ab43882b7157b08e5a52cb3f40392ab72aced2
Mathias Arzberger <develop@pdir.de>
2017-07-30 10:41:31 +0200

    add contao manager support

---
+++ src/ContaoManagerPlugin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * mobilede for Contao Open Source CMS
+ *
+ * Copyright (C) 2017 pdir / digital agentur <develop@pdir.de>
+ *
+ * @package    mobilede
+ * @link       https://pdir.de/mobilede
+ * @license    pdir license - All-rights-reserved - commercial extension
+ * @author     pdir GmbH <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ContaoDD\AdvancedClassesBundle;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class ContaoManagerPlugin implements BundlePluginInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getBundles(ParserInterface $parser)
+	{
+		return [
+			BundleConfig::create(AdvancedClassesBundle::class)
+				->setLoadAfter([ContaoCoreBundle::class])
+				->setReplace(['advanced_classes']),
+		];
+	}
+}
