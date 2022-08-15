8357bb3d66f0cca4b71dc137d795ed689995ef04
Mathias Arzberger <develop@pdir.de>
2022-08-15 20:50:58 +0200

    add bundle

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -19,6 +19,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoDD\AdvancedClassesBundle\ContaoDDAdvancedClassesBundle;
 
 class Plugin implements BundlePluginInterface
 {
